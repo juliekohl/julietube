fa1903378020f29203f5bc621ed061bf62a5a944
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-10 12:01:22 -0300

    Final da aula 3º aula.

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,20 +1,70 @@
 import React from "react";
 import {StyledRegisterVideo} from "./styles";
 
-export default function RegisterVideo () {
+function useForm(propsForm) {
+    const [ values, setValues ] = React.useState(propsForm.initialValues);
+
+    return {
+        values,
+        handleChange: (event) => {
+            const value = event.target.value;
+            const name = event.target.name;
+            setValues({
+                ...values,
+                [name]: value,
+            });
+        },
+        clearForm() {
+            setValues({});
+        }
+    };
+}
+
+export default function RegisterVideo() {
+    const formRegistration = useForm({
+        initialValues: { title: "", url: "" }
+    });
     const [ formVisible, setFormVisible ] = React.useState(false);
 
     return (
         <StyledRegisterVideo>
-            <button className="add-video" >+</button>
-            <form>
-                <div>
-                    <button className="close-modal" >x</button>
-                    <input placeholder="Titulo do vídeo" />
-                    <input placeholder="URL" />
-                    <button type="submit" >Cadastrar</button>
-                </div>
-            </form>
+            <button
+                className="add-video"
+                type="button"
+                onClick={() => setFormVisible(true)}
+            >+</button>
+            {formVisible
+                ? (
+                    <form
+                        onSubmit={(event) => {
+                        event.preventDefault();
+                        console.log(formRegistration.values);
+
+                        setFormVisible(false);
+                        formRegistration.clearForm();
+                    }}>
+                        <div>
+                            <button
+                                className="close-modal"
+                                onClick={() => setFormVisible(false)}
+                            >x</button>
+                            <input
+                                placeholder="Titulo do vídeo"
+                                name="title"
+                                value={formRegistration.values.title}
+                                onChange={formRegistration.handleChange}
+                            />
+                            <input
+                                placeholder="URL"
+                                name="url"
+                                value={formRegistration.values.url}
+                                onChange={formRegistration.handleChange}
+                            />
+                            <button type="submit">Cadastrar</button>
+                        </div>
+                    </form>
+                )
+                : false}
         </StyledRegisterVideo>
     )
 }

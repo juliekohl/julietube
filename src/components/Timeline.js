50cafcb23c215cfd7fb896681f5dc2d211dfde03
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-11 16:04:24 -0300

    Fix redirect.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export default function Timeline({searchValue, ...props}) {
     const playlistNames = Object.keys(props.playlists);
-    const favorites = props.favorites;
-    let countVideos = 0;
 
     return (
         <StyledTimeline>
@@ -19,43 +17,14 @@ export default function Timeline({searchValue, ...props}) {
                                     const searchValueNormalized = searchValue.toLowerCase();
                                     return titleNormalized.includes(searchValueNormalized)
                                 }).map((video) => {
-                                    countVideos++;
-                                    let video_id = "";
-
-                                    if (video.url.includes(".be/")) {
-                                        video_id = video.url.split(".be/")[1];
-                                    }
-
-                                    if (video.url.includes("/embed/")) {
-                                        video_id = video.url.split("/embed/")[1];
-                                    }
-
-                                    if (video.url.includes("com/v/")) {
-                                        video_id = video.url.split("com/v/")[1];
-                                    }
-
-                                    if (video.url.includes("/watch?v=")) {
-                                        video_id = video.url.split("/watch?v=")[1];
-                                        let ampersandPosition = video_id.indexOf("&");
-                                        if (ampersandPosition != -1) {
-                                            video_id = video_id.substring(0, ampersandPosition);
-                                        }
-                                    }
-
                                     return (
-                                        <a key={video.url} href={{
-                                            pathname: "/video",
-                                            query: {
-                                                id: video.url,
-                                                title: video.title,
-                                            },
-                                        }}>
+                                        <a key={video.url} href={video.url}>
                                             <img src={video.thumb} />
                                             <span>{video.title}</span>
                                         </a>
                                     );
+
                                 })}
-                            {countVideos === 0 ? "Nenhum vídeo encontrado." : ""}
                         </div>
                     </section>
                 )
@@ -68,7 +37,6 @@ export const StyledTimeline = styled.div`
   flex: 1;
   width: 100%;
   padding: 16px;
-  // overflow: hidden;
   h2 {
     margin-bottom: 16px;
     font-size: 16px;
